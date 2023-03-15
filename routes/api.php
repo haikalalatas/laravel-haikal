2bc16d7a7b8cbb8de2b834902226ba385206eb00
haikalalatas <aidel21si@mahasiswa.pcr.ac.id>
2023-03-15 13:13:08 +0700

    new repo

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+//Route::apiResource('/posts',App\Http\Controllers\Api\PostController::class)->middleware('hak-akses');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    Route::apiResource('/posts',
+    App\Http\Controllers\Api\PostController::class);
+});
+Route::post('login',
+[App\Http\Controllers\Api\AuthController::class, 'signin']);
+Route::post('register',
+[App\Http\Controllers\Api\AuthController::class, 'signup']);
+Route::get('/posts',[App\Http\Controllers\Api\PostController::class,'index'])->middleware(['auth:sanctum', 'hak-akses:admin']);
+Route::get('/posts/:id', [App\Http\Controllers\Api\PostController::class,'show'])->middleware('auth:sanctum');
